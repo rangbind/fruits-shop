85bd4bd012e250717518f496d1337605b05d1b4f
Rang <rbind403@gmail.com>
2021-10-15 18:56:12 +0530

    updated dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,8 +7,7 @@
 	$dataArray = [];
 	while ($row = mysqli_fetch_assoc($result)) {
 		$dataArray[] = $row;
-	}	
-	//print_r($dataArray);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,33 +19,56 @@
 </head>
 
 <body>
-	<h3>Showing fruit_master Details</h3>
+	<h3>Fruit List</h3>
 	<table class="table table-hover" style="width: 50%">
 		<thead>
 			<tr>
-				<th>ID</th>
-				<th>fruit_name</th>
+				<th>Id</th>
+				<th>Fruit name</th>
 				<th>Price</th>
 				<th>Quantity</th>
-
+				<th>Created At</th>
 			</tr>
 		</thead>
 		<tbody>
+			
 			<?php
+
+			$price_sum = 0;
+			$quantity_sum = 0;
+
 			foreach ($dataArray as $key => $value) {
+
+				$price_sum = $price_sum + $value['price'];
+				$quantity_sum = $quantity_sum + $value['quantity'];
+
 			?>
+			
+			
 			<tr>
 				<td><?php echo $value['id']; ?></td>
 				<td><?php echo $value['fruit_name']; ?></td>
 				<td><?php echo $value['price']; ?></td>
 				<td><?php echo $value['quantity']; ?></td>
-
+				<td><?php echo $value['created_at']; ?></td>
+				
 			</tr>
+
 			<?php	
 			}
+
 			?>
+
+			<tr>
+				<th> <?php echo "total"; ?> </th>
+				<th> <?php echo count($dataArray); ?> </th>
+				<th> <?php echo $price_sum; ?> </th>
+				<th> <?php echo $quantity_sum; ?> </th>
+				<th></th>
+			</tr>
+			
 			
 		</tbody>
 	</table>
 </body>
-</html>
+</html>	
