4c12eb3fcb00072333459fda036935c6d4a12eec
Rang <rbind403@gmail.com>
2021-10-19 21:50:22 +0530

    updated files

--- create_Accountview.php
+++ create_Accountview.php
@@ -1,5 +1,7 @@
 <?php
 	
+	include('header.php');
+	
 ?>
 
 <h3> Login Here </h3><br>
@@ -7,25 +9,24 @@
 	<div class="col-sm-6">
 		<form method="post" action="server.php">
 			<label> <b>Name:</b></label>
-			<input type="textbox" class="form-control" name="name" placeholder="name" required="true"><br><br>	
+			<input type="textbox" class="form-control" name="name" placeholder="name" required="true"><br>
 				
 			<label> <b>Phone No:</b></label>		
-			<input type="textbox" class="form-control" name="phone_no" placeholder="phone_no" required="true"><br><br>
+			<input type="textbox" class="form-control" name="phone" placeholder="phone" required="true"><br>
 			
 			<label> <b>Email:</b></label>
-			<input type="textbox" class="form-control" name="email" placeholder="email" required="true"><br><br>
+			<input type="textbox" class="form-control" name="email" placeholder="email" required="true"><br>
 
 			<label> <b>Address:</b></label>
-			<input type="textbox" class="form-control" name="address" placeholder="address" required="true"><br><br>
+			<input type="textbox" class="form-control" name="address" placeholder="address" required="true"><br>
 			
 			<label> <b>Password:</b></label>
-			<input type="textbox" class="form-control" name="password" placeholder="password" required="true"><br><br>
+			<input type="textbox" class="form-control" name="password" placeholder="password" required="true"><br>
 			
 			<label> <b>Confirm Password:</b></label>
 			<input type="textbox" class="form-control" name="confirmed_password" placeholder="confirmed_password" required="true"><br>
 			
-			<br>
-			<button class="btn btn-primary" type="submit" name="submit"> <b><h2> Submit </h2></b> </button>
+			<button class="btn btn-primary" type="submit" name="submit"> <b> Submit </b> </button>
 			
 		</form>
 	</div>	
--- database.sql
+++ database.sql
@@ -12,3 +12,32 @@ CREATE TABLE `fruit_master` (
   `last_updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+
+CREATE TABLE `Registration_page` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `phone_no` varchar(10) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address` varchar(60) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  `confirmed_password` varchar(20) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
