986f050d9083849f206495e6881b90d067e73071
Rang <rbind403@gmail.com>
2021-10-19 22:47:58 +0530

    updated files

---
+++ accountList.php
@@ -0,0 +1,72 @@
+<?php
+	include('header.php');
+
+	$selectquery = "select * from Registration_page";
+	$result = $mysqli->query($selectquery);
+
+	$dataArray = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$dataArray[] = $row;
+	}
+?>
+<h3>Account List</h3>
+<table class="table table-hover" style="width: 80%">
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>name</th>
+			<th>phone</th>
+			<th>email</th>
+			<th>address </th>
+			<th>password</th>
+			<th>confirmed_password</th>
+			<th>created_at</th>
+			<th>Action</th>
+
+		</tr>
+
+	</thead>
+	<tbody>
+		
+		<?php
+
+		foreach ($dataArray as $key => $value) {
+
+		?>
+		<tr>
+			<td><?php echo $value['id']; ?></td>
+			<td><?php echo $value['name']; ?></td>
+			<td><?php echo $value['phone_no']; ?></td>
+			<td><?php echo $value['email']; ?></td>
+			<td><?php echo $value['address']; ?></td>
+			<td><?php echo $value['password']; ?></td>
+			<td><?php echo $value['confirmed_password']; ?></td>
+			<td><?php echo $value['created_at']; ?></td>
+			<td><?php echo $value['Action']; ?></td>
+
+
+	
+			<!--<th> 
+				<a class="btn btn-outline-danger" href="deleteFruit.php?fruit_id=<?php echo $value['id'];?>" id="fruit_id"> delete </a> <a class="btn btn-primary" href="editfruitview.php?fruit_id=<?php echo $value['id'];?>" id="fruit_id"> edit </a>
+			</th>--> 
+			
+		</tr>
+
+		<?php	
+		}
+		?>
+
+		<tr>
+			<th> <?php echo "total"; ?> </th>
+			<th> <?php echo count($dataArray); ?> </th>
+			
+			
+
+
+			
+
+
+			
+		</tr>
+	</tbody>
+</table>
