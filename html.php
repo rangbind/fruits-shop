f227708f1299a0448dd0826ef679f4cf3bb30bc0
Rang <rbind403@gmail.com>
2021-10-27 21:14:08 +0530

    updated files

---
+++ html.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> html tags practice </title>
+</head>
+
+<body>
+
+	<h3> html tags </h3>
+
+	<p>Mohandas Karamchand Gandhi (/ˈɡɑːndi, ˈɡændi/;[2] 2 October 1869 – 30 January 1948) was an Indian lawyer,[3] anti-colonial nationalist[4] and political ethicist[5] who employed nonviolent resistance to lead the successful campaign for India's independence from British rule[6] and in turn to inspire movements for civil rights and freedom across the world. The honorific Mahātmā (Sanskrit: "great-souled", "venerable"), first applied to him in 1914 in South Africa, is now used throughout the world.[7][8]<br>
+	<div>Born and raised in a Hindu family in coastal Gujarat, Gandhi trained in the law at the Inner Temple, London, and was called</div> <div>to the bar at age 22 in June 1891. After two uncertain years in India, where he was unable to start a successful law</div> practice, he moved to South Africa in 1893 to <b>represent an Indian merchant in a lawsuit.</b> He went on to live in South Africa for 21 years. It was here that Gandhi raised a family and first employed nonviolent resistance in a campaign for civil 
+	rights.<br>
+	<span>In 1915, aged 45, he returned to India. &nbsp; &nbsp;&nbsp;&nbsp;He set about organising peasants, farmers, and urban labourers to protest against</span>
+	<br>
+	excessive land-tax and discrimination. Assuming leadership of the Indian National Congress in 1921, Gandhi led nationwide campaigns for easing poverty, expanding women's rights, building religious and ethnic amity, ending untouchability, and above all for achieving swaraj or self-rule.[9]</p>
+
+	
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/_FNOyXl15MI?start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+
+	<br><br><br>
+	<br><br><br>
+	&#8508;
+	<br><br><br>
+	
+	&#128516;
+
+
+
+	<h3> end </h3>
+
+
+	<form enctype="multipart/form-data"> 
+
+		<input type="file" multiple="true">
+	</form>
+
+
+
+
+
+
+</body>
+</html>
